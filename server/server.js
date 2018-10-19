e2e43babee0460082382a9edd23d8fc5988f2ef6
daedalus1948 <daedalus1948@gmail.com>
2018-10-19 11:53:40 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,9 +12,9 @@ const chatApp = new Chat(io, new RedisService(redis));
 chatApp.initialize()
     .then((status) => { // chat app initialized, injected dataService succesfully fetched message history
         console.log("chat app initialized");
-        httpServer.listen(4000, ()=>{
-            // server bound to port 4000 and listening
-            console.log("serving on 4000");
+        httpServer.listen(3999, ()=>{
+            // server bound to port 3999 and listening
+            console.log("serving on 3999");
         });
     })
     .catch((error)=>{
