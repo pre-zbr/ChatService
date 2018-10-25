f7debd904b43afb8f82b3e9e6ad391952bd0d2b8
daedalus1948 <daedalus1948@gmail.com>
2018-10-25 20:16:02 +0200

    Update message.js

--- server/core/message.js
+++ server/core/message.js
@@ -1,6 +1,9 @@
+const crypto = require('crypto');
+
 module.exports = class Message {
     
     constructor(userId, color, data) {
+        this._uid = crypto.randomBytes(10).toString('hex');
         this.userId = userId;
         this.timestamp = new Date();
         this.color = color;
